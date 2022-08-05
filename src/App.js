fe2e9a3a048e325d1f98dd071a8de6c9a7c6c6b9
Harrison <harrisonderick.65@gmail.com>
2022-08-05 15:59:27 +0100

    Page title changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React, { Component } from 'react';
 import SearchBox from './components/SearchBox';
 import Card from './components/Card';
 import users from './components/users.json';
 import './App.css';
 import 'tachyons';
 
-import React, { Component } from 'react';
 class App extends Component {
   constructor(){
     super();
@@ -12,24 +12,28 @@ class App extends Component {
       users: users
     }
   }
-  updateSearchInput = (inputContent) => {
-    // this.setState({});
+  // users = [];
+  // fetch('http://jsonplaceholder.typicode.com/users')
+  // .then((response) => (response.json()))
+  // .then((response) => (users = response)
+  // .catch((err) => (console.log(err)))
+  
+  updateSearchInput = (searchContent) => {
     this.setState({
       users: users.filter((user) => (
-        user.name.toLowerCase().includes(inputContent.toLowerCase())
+        user.name.toLowerCase().includes(searchContent.toLowerCase())
       ))
     });
-    console.log(this.state.users);
-    console.log(inputContent.toLowerCase()); 
   }
+
   render(){
-  return (
-    <div> 
-      <SearchBox updateSearchInput={this.updateSearchInput} />
-      <Card users={this.state.users} />
-    </div>
-  );
-}
+    return (
+      <div>
+        <SearchBox updateSearchInput={this.updateSearchInput} />
+        <Card users={this.state.users} />
+      </div>
+    );
+  }
 }
 
 export default App;
