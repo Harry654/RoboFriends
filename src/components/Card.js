742fa8f8ca840e109bee9654f806092aaad6846c
Harrison Akunne <harrisonderick.65@gmail.com>
2022-08-18 22:50:44 +0100

    feat: users have been reversed!

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({users}) {
     // console.log(users.reverse());
     return(
-            users.map.reverse()((user) => ( 
+            users.reverse().map((user) => ( 
                 <div key={user.id} className="card">
                     <img src={`https://robohash.org/${user.id}?set=set4`} alt={user.name} />
                     <h4>{user.name}</h4>
